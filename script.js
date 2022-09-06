494a7fb4c100d541393a9d4af08c6b68abd56bcc
krdelva <kevin.r.delva@gmail.com>
2022-09-06 13:04:25 -0400

    Add style to dynamic content, plus cursor on hover

--- script.js
+++ script.js
@@ -23,14 +23,34 @@ paperDiv.addEventListener("click", () => game("paper", getComputerChoice()), fal
 scissorsDiv.addEventListener("click", () => game("scissors", getComputerChoice()), false);
 
 // Modal button event listener, restarts the match
-modalButton.addEventListener("click", () => {
+modalButton.addEventListener("click", () => { 
+    // Resets score to 0
     playerScore.children[0].innerHTML = 0;
     computerScore.children[0].innerHTML = 0;
+    
+    // Resets the player and computer's choices to blank
     playerCurrentChoice.children[1].innerHTML = ``;
     computerCurrentChoice.children[1].innerHTML = ``;
+    
+    // Resets current winner to blank
     currentWinner.innerHTML = ``;
+    
+    // Closes overlay and modal
     modal.style.display = "none";
     overlay.style.display = "none";
+
+
+    // Resets background and text color to initial values  of the player and computer's choice
+    playerCurrentChoice.children[1].style.backgroundColor = "white";
+    playerCurrentChoice.children[1].style.color = "black";
+    computerCurrentChoice.children[1].style.backgroundColor = "white";
+    computerCurrentChoice.children[1].style.color = "black";
+
+    // Resets background and text color to initial values  of the player and computer's score
+    playerScore.children[0].style.backgroundColor = "white";
+    playerScore.children[0].style.color = "black";
+    computerScore.children[0].style.backgroundColor = "white";
+    computerScore.children[0].style.color = "black";
 });
 
 // Function to randoize the computer's choice
@@ -58,29 +78,79 @@ function game(playerChoice, computerChoice) {
 
     // Game logic, conditionals to determine who won the current hand
     if (playerChoice === computerChoice) {
+        // Adds text to the currentWinner element, to show current hand is a tie
         currentWinner.innerHTML = "It's a tie!!";
+
+        // Resets background and text color to initial values of the player and computer's choice
+        playerCurrentChoice.children[1].style.backgroundColor = "white";
+        playerCurrentChoice.children[1].style.color = "black";
+        computerCurrentChoice.children[1].style.backgroundColor = "white";
+        computerCurrentChoice.children[1].style.color = "black";
+
     } else if (
         (playerChoice === "rock" && computerChoice === "scissors") ||
         (playerChoice === "paper" && computerChoice === "rock") ||
         (playerChoice === "scissors" && computerChoice === "paper")
     ) {
+
+        // Adds text to the currentWinner element, to show player won current hand
         currentWinner.innerHTML = "Player wins hand!!";
+        
+        // Adds 1 to the player's score
         playerScore.children[0].innerHTML++;
+
+        // Adds style to the players's choice to show their choice is the winner
+        playerCurrentChoice.children[1].style.backgroundColor = "black";
+        playerCurrentChoice.children[1].style.color = "white";
+
+        // Resets the computer choice's style
+        computerCurrentChoice.children[1].style.backgroundColor = "white";
+        computerCurrentChoice.children[1].style.color = "black";
+
     } else if (
         (computerChoice === "rock" && playerChoice === "scissors") ||
         (computerChoice === "paper" && playerChoice === "rock") ||
         (computerChoice === "scissors" && playerChoice === "paper")
     ) {
-        currentWinner.innerHTML = "Computer wins hand!!"
+
+        // Adds text to the currentWinner element, to show computer won current hand
+        currentWinner.innerHTML = "Computer wins hand!!";
+        
+        // Adds 1 to the computer's score
         computerScore.children[0].innerHTML++;
+
+        // Adds style to the computer's choice to show their choice is the winner
+        computerCurrentChoice.children[1].style.backgroundColor = "black";
+        computerCurrentChoice.children[1].style.color = "white";
+
+        // Resets the player choice's style
+        playerCurrentChoice.children[1].style.backgroundColor = "white";
+        playerCurrentChoice.children[1].style.color = "black";
     }
 
     // Conditionals to determine if match is over
     if (playerScore.children[0].innerHTML == 5) {
+
+        // Adds text to score, to show player won the match
         playerScore.children[0].innerHTML = "Player Wins!";
+
+        // Adds style to score div, to show player won
+        playerScore.children[0].style.backgroundColor = "black";
+        playerScore.children[0].style.color = "white";
+
+        // Calls function to show overlay/modal
         endGame();
+
     } else if (computerScore.children[0].innerHTML == 5) {
+
+        // Adds text to score, to show computer won the match
         computerScore.children[0].innerHTML = "Computer Wins!";
+
+         // Adds style to score div, to show computer won
+        computerScore.children[0].style.backgroundColor = "black";
+        computerScore.children[0].style.color = "white";
+
+        // Calls function to show overlay/modal
         endGame();
     }
 
